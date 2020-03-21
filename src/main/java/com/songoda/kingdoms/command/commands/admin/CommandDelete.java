54580a3f639723108fad1a8dd73b636f22af4bbf
Curtis73 <mousiearmyinfo@gmail.com>
2020-03-21 20:02:39 +0000

    Clean up imports

--- src/main/java/com/songoda/kingdoms/command/commands/admin/CommandDelete.java
+++ src/main/java/com/songoda/kingdoms/command/commands/admin/CommandDelete.java
@@ -2,7 +2,6 @@ package com.songoda.kingdoms.command.commands.admin;
 
 import java.util.Optional;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
